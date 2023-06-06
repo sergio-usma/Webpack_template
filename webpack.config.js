3804348fb328cfe262ef5039cd846edded222661
Sergio Andres Usma <126805818+sergio-usma@users.noreply.github.com>
2023-06-06 17:02:21 -0500

    Update webpack.config.js to save images in 'assets' folder

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]',
+        },
       },
     ],
   },
